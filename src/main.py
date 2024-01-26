45bbdb40b34bc260c8d1816b7b364adbae5c79f1
victord03 <vic.kaklamanis@gmail.com>
2024-01-27 00:48:48 +0200

    Created the classes.

--- src/main.py
+++ src/main.py
@@ -2,15 +2,44 @@
 from datetime import datetime
 
 
+class Book:
+    title: str
+    category: str
+    author: str
+    year: int
+    page_count: int
+
+    def __init__(self, title, category, author, year, page_count):
+        self.title = title
+        self.category = category
+        self.author = author
+        self.year = year
+        self.page_count = page_count
+
+
 class Event:
     """Inbound query"""
+
     timestamp: datetime
     customer: id
     book_title: str
     price: float
     address: str
 
+    def __init__(self, timestamp, customer, book_title, price, address):
+        self.timestamp = timestamp
+        self.customer = customer
+        self.book_title = book_title
+        self.price = price
+        self.address = address
+
 
 class EventStore:
     """Write DB"""
-    ...
+
+    metadata: list
+    payload: str
+
+    def __init__(self, metadata, payload):
+        self.metadata = metadata
+        self.payload = payload
--- test/test_main.py
+++ test/test_main.py
@@ -1,17 +1,41 @@
+from datetime import datetime
+
 from src import main
 import pytest
 
-pytest_mark = pytest.mark.parametrize(
-
-)
+pytest_mark = pytest.mark.parametrize()
 
 
 def setup_event():
-    return main.Event()
+    timestamp = datetime.now()
+    customer = 1
+    book_title = ""
+    price = 0.0
+    address = ""
+    return main.Event(
+        timestamp=timestamp,
+        customer=customer,
+        book_title=book_title,
+        price=price,
+        address=address,
+    )
 
 
 def setup_event_store():
-    return main.EventStore()
+    metadata = []
+    payload = ""
+    return main.EventStore(metadata=metadata, payload=payload)
+
+
+def setup_book():
+    title = ""
+    category = ""
+    author = ""
+    year = 00
+    page_count = 00
+    return main.Book(
+        title=title, category=category, author=author, year=year, page_count=page_count
+    )
 
 
 def test_class_event():
@@ -23,3 +47,7 @@ def test_class_event_store():
     res = setup_event_store()
     assert isinstance(res, main.EventStore)
 
+
+def test_class_book():
+    res = setup_book()
+    assert isinstance(res, main.Book)
